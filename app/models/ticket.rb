0c29a1fc2b5d4e4a98dc4a1ae0d395bdd4105f0f
Anand Upadhyay <anandu@gumromedia.com>
2015-12-07 11:41:34 -0500

    used enum for status

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
 
+  enum status: [ :active, :waiting, :done, :trashed ]
+
   scope :allopen, -> {where('status = 0 or status = 1')}
-  scope :status, ->(status) {where("status = ?", status)}
   scope :priority, -> {where(priority: true)}
 end
