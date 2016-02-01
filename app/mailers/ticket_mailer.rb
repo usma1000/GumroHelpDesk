f36c078083b2204ae470ce4b36707eb8240558f8
Anand Upadhyay <anandu@gumromedia.com>
2016-02-01 16:35:52 -0500

    changed email to info@ from anandu@

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -2,6 +2,6 @@ class TicketMailer < ApplicationMailer
   default from: "helpdesk@gumroandassociates.com"
   def new_ticket_email(ticket)
     @ticket = ticket
-    mail(to: 'anandu@gumroandassociates.com', subject: "New Ticket: #{@ticket.subject}")
+    mail(to: 'info@gumroandassociates.com', subject: "New Ticket: #{@ticket.subject}")
   end
 end
