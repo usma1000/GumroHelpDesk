097274d352dd8aff05b76666342485cc7e72f627
Anand Upadhyay <anandu@gumromedia.com>
2016-02-01 15:06:41 -0500

    mail settings

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,4 +1,5 @@
 class TicketMailer < ApplicationMailer
+  default from: "helpdesk@gumroandassociates.com"
   def new_ticket_email(ticket)
     @ticket = ticket
     mail(to: 'anandu@gumroandassociates.com', subject: "New Ticket: #{@ticket.subject}")
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   #Configuration for sending email
   ActionMailer::Base.smtp_settings = {
