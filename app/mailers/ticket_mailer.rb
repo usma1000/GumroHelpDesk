3ac5c2eb5c2e4f23ddc178081e70eef9c8fb5a7d
Anand Upadhyay <anandu@gumromedia.com>
2016-02-02 15:28:27 -0500

    trying to send email to both me and ryan

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -3,5 +3,6 @@ class TicketMailer < ApplicationMailer
   def new_ticket_email(ticket)
     @ticket = ticket
     mail(to: 'anandu@gumroandassociates.com', subject: "New Ticket: #{@ticket.subject}")
+    mail(to: 'rgumro@gumroandassociates.com', subject: "New Ticket: #{@ticket.subject}")
   end
 end
