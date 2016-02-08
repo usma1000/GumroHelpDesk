2ada31861f570cf6bd3273c94494780c47f85712
Anand Upadhyay <anandu@gumromedia.com>
2016-02-08 08:40:48 -0500

    added a cc field to emails

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -3,8 +3,9 @@ class TicketMailer < ApplicationMailer
 
   def new_ticket_email(ticket)
     @ticket = ticket
+    cc_email = @ticket.user.email
     @subject = "New Ticket - " + @ticket.subject
 
-    mail(to: 'info@gumroandassociates.com', subject: @subject)
+    mail(to: 'info@gumroandassociates.com', cc: cc_email, subject: @subject)
   end
 end
