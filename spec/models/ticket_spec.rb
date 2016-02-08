4c9296449d1299dede5423f9976f51216f1e1820
Anand Upadhyay <anandu@gumromedia.com>
2016-02-08 09:32:57 -0500

    added specs

---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Ticket do
+  it "has a valid factory"
+  it "is invalid without a subject"
+  it "is invalid without a content"
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe User do
+  it "has a valid factory"
+end
