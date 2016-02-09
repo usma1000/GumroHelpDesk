a704607b9b3f7e1c0eac109ef9c4e80b6027d983
Anand Upadhyay <anandu@gumromedia.com>
2016-02-09 16:59:29 -0500

    added tickets today count

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,5 +4,6 @@ class StatisticsController < ApplicationController
     @tickets_this_month = Ticket.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month)
     @tickets_this_week = Ticket.where(:created_at => Time.now.beginning_of_week..Time.now.end_of_week)
     @tickets_last_month = Ticket.where(:created_at => Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month)
+    @tickets_today = Ticket.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
   end
 end
